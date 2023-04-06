bbfeb83d1f46ab3b569facc00b1a361fa598e8ab
diffloc <6632742+diffloc@users.noreply.github.com>
2023-04-06 12:41:42 -0500

    Stage 2/7 complete
    Instructions were not clear regarding '0' padding

--- Main.java
+++ Main.java
@@ -4,9 +4,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Input string:");
+        // String userInput = scanner.nextLine();
+        //
+        // for (int i = 0; i < userInput.length(); i++) {
+        //     System.out.println(userInput.charAt(i) + " = " + Integer.toBinaryString(userInput.charAt(i)));
+        //
+        // }
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input string:");
-        System.out.println(scanner.nextLine().replaceAll("", " ").replaceFirst(" ", ""));
+        String str = scanner.nextLine();
+        System.out.println();
+        System.out.println("The result:");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i) + " = " + String.format("%7s", Integer.toBinaryString(str.charAt(i))).replace(" ", "0"));
+        }
 
     }
 }
