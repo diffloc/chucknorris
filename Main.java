735b5c3837326bddd653320d1a91f28923e401ed
diffloc <6632742+diffloc@users.noreply.github.com>
2023-04-06 15:02:38 -0500

    Stage 3/5 complete

--- Main.java
+++ Main.java
@@ -4,23 +4,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.println("Input string:");
-        // String userInput = scanner.nextLine();
-        //
-        // for (int i = 0; i < userInput.length(); i++) {
-        //     System.out.println(userInput.charAt(i) + " = " + Integer.toBinaryString(userInput.charAt(i)));
-        //
-        // }
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input string:");
         String str = scanner.nextLine();
         System.out.println();
         System.out.println("The result:");
+
+        String binary = "";
+        StringBuilder chuckNorris = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
-            System.out.println(str.charAt(i) + " = " + String.format("%7s", Integer.toBinaryString(str.charAt(i))).replace(" ", "0"));
+            binary = String.format("%7s", Integer.toBinaryString(str.charAt(i))).replace(" ", "0");
+            chuckNorris.append(binary);
+            // System.out.println(str.charAt(i) + " = " + binary);
         }
+
+        String newBinary = chuckNorris.toString().replaceAll("([01])(?!\\1)", "$1 ");
+        String[] parts = newBinary.split(" ");
+
+        for (int i = 0; i < parts.length; i++) {
+            StringBuilder series = new StringBuilder();
+            for (int j = 0; j < parts[i].length(); j++) {
+                series.append("0");
+            }
+            if (parts[i].charAt(0) == '0') {
+                parts[i] = "00 " + series;
+            } else {
+                parts[i] = "0 " + series;
+            }
+        }
+        for (int i = 0; i < parts.length; i++) {
+            System.out.print(parts[i] + " ");
+        }
+
+
 
     }
 }
