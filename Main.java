1112931a4f4285977418cb4bcb684e41991a99fa
diffloc <6632742+diffloc@users.noreply.github.com>
2023-04-07 14:21:34 -0500

    Stage 5/5 complete

--- Main.java
+++ Main.java
@@ -5,25 +5,58 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // encode(scanner);
-        decode(scanner);
+
+        String operation;
+        do {
+            System.out.println("\nPlease input operation (encode/decode/exit):");
+            operation = scanner.nextLine();
+            switch (operation) {
+                case "encode" -> encode(scanner);
+                case "decode" -> decode(scanner);
+                case "exit" -> System.out.println("Bye!");
+                default -> System.out.println("There is no '" + operation + "' operation");
+            }
+        } while (!operation.equals("exit"));
+
     }
 
     public static void decode(Scanner scanner) {
 
         System.out.println("Input encoded string:");
         String str = scanner.nextLine();
-        System.out.println();
-        System.out.println("The result:");
+
+        if (!str.matches("^[ 0]*$")) {
+            System.out.println("Encoded string is not valid.");
+            return;
+        }
+
+
 
         String[] substrings = str.split(" ");
+
+        if (substrings.length % 2 != 0) {
+            System.out.println("Encoded string is not valid.");
+            return;
+        }
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < substrings.length; i +=2) {
             String leftPart = substrings[i];
             String rightPart = substrings[i + 1];
+
+            if (!leftPart.equals("0") && !leftPart.equals("00")) {
+                System.out.println("Encoded string is not valid.");
+                return;
+            }
+
             String decodeLeftPart = leftPart.equals("00") ? "0" : "1";
             String buildRightPart = new String(new char[rightPart.length()]).replace("\0", decodeLeftPart);
             sb.append(buildRightPart);
+        }
+
+        if (sb.length() % 7 != 0) {
+            System.out.println("Encoded string is not valid.");
+            return;
         }
 
         StringBuilder ascii = new StringBuilder();
@@ -32,6 +65,8 @@ public class Main {
             char asciiChar = (char) Integer.parseInt(byteStr, 2);
             ascii.append(asciiChar);
         }
+
+        System.out.println("Decoded string:");
         System.out.println(ascii.toString());
 
     }
@@ -39,8 +74,8 @@ public class Main {
     public static void encode(Scanner scanner) {
         System.out.println("Input string:");
         String str = scanner.nextLine();
-        System.out.println();
-        System.out.println("The result:");
+
+
 
         String binary = "";
         StringBuilder chuckNorris = new StringBuilder();
@@ -63,8 +98,10 @@ public class Main {
                 parts[i] = "0 " + series;
             }
         }
+        System.out.println("Encoded string::");
         for (int i = 0; i < parts.length; i++) {
             System.out.print(parts[i] + " ");
         }
+        System.out.println();
     }
 }
